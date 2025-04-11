5c15fec46b4d921965011a05952ff8ccd80d199f
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-04-11 10:06:33 -0300

    feat: add value box import

--- src/dashboard/dashboard.jsx
+++ src/dashboard/dashboard.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { Component } from "react";
+import ValueBox from "../widget/valueBox";
 
-export default function Dashboard(props) {
-    return <div>
-        <h1>Dashboard</h1>
-    </div>
-}
+class Dashboard extends Component {
+    render() {
+      return (
+        <div>
+      
+          <ValueBox
+            cols='12 4'
+            color='green'
+            icon='bank'
+            value='R$ 10'
+            text='Total de Créditos'
+          />
+          <ValueBox
+            cols='12 4'
+            color='red'
+            icon='credit-card'
+            value='R$ 10'
+            text='Total de Débitos'
+          />
+          <ValueBox
+            cols='12 4'
+            color='blue'
+            icon='money'
+            value='R$ 0'
+            text='Valor Consolidado'
+          />
+        </div>
+      );
+    }
+  }
+  
+  export default Dashboard;
