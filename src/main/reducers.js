ccc774862c4179b32df996a3c44b3bad127510a9
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-04-11 11:51:23 -0300

    feat: add Dashboard Reducer

--- src/main/reducers.js
+++ src/main/reducers.js
@@ -1,23 +1,9 @@
 import { combineReducers } from 'redux'
 
-const initialState = {
-  summary: {
-    credit: 100,
-    debt: 0,
-  }
-}
-
-function dashboardReducer(state = initialState, action) {
-  switch (action.type) {
-    case 'DASHBOARD_SUMMARY_FETCHED':
-      return { ...state, summary: action.payload }
-    default:
-      return state
-  }
-}
+import DashboardReducer from '../dashboard/dashboardReducer'
 
 const rootReducer = combineReducers({
-  dashboard: dashboardReducer
+  dashboard: DashboardReducer
 })
 
 export default rootReducer
