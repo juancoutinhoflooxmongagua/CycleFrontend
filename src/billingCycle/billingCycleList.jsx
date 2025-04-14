3abfa0860a20f2d2892e92686916d41d635884f7
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-04-14 10:38:15 -0300

    feat: finish method list in project

--- src/billingCycle/billingCycleList.jsx
+++ src/billingCycle/billingCycleList.jsx
@@ -1,6 +1,24 @@
 import React, { Component } from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import { getList } from './billingCyclesAction'
 
 class BillingCycleList extends Component {
+  componentDidMount() {
+    this.props.getList()
+  }
+
+  renderRows() {
+    const list = this.props.billingCycle.list || []
+    return list.map((item, index) => (
+      <tr key={index}>
+        <td>{item.name}</td>
+        <td>{item.month}</td>
+        <td>{item.year}</td>
+      </tr>
+    ))
+  }
+
   render() {
     return (
       <div>
@@ -13,7 +31,7 @@ class BillingCycleList extends Component {
             </tr>
           </thead>
           <tbody>
-            
+            {this.renderRows()}
           </tbody>
         </table>
       </div>
@@ -21,4 +39,11 @@ class BillingCycleList extends Component {
   }
 }
 
-export default BillingCycleList
+const mapStateToProps = state => ({
+  billingCycle: state.billingCycle
+})  
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators({ getList }, dispatch)
+
+export default connect(mapStateToProps, mapDispatchToProps)(BillingCycleList)
