4e0f812a8530a1c2d0c785ee1239ee52f7535100
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-04-14 10:24:32 -0300

    feat: billing cycle Component

--- src/main/reducers.js
+++ src/main/reducers.js
@@ -2,9 +2,12 @@ import { combineReducers } from 'redux'
 
 import DashboardReducer from '../dashboard/dashboardReducer'
 import tabReducer from '../common/tab/tabReducer'
+import BillingCycleReducer from '../billingCycle/billingCycleReducer'
+
 const rootReducer = combineReducers({
-  dashboard: DashboardReducer,
-  tab: tabReducer
+    dashboard: DashboardReducer,
+    tab: tabReducer,
+    billingCycle: BillingCycleReducer
 })
 
 export default rootReducer
