aa30b3e8cef6051155ff22c152cedc314c3c2252
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-04-15 09:47:16 -0300

    fix: items

--- src/index.jsx
+++ src/index.jsx
@@ -3,18 +3,16 @@ import ReactDOM from 'react-dom';
 import Routes from './main/routes';
 import { applyMiddleware, createStore } from 'redux';
 import promise from 'redux-promise';
-import thunk from 'redux-thunk'; // Importando o redux-thunk
+import thunk from 'redux-thunk'; 
 import { Provider } from 'react-redux';
 import App from './main/app';
 import reducers from './main/reducers';
 
-// Configurando DevTools
 const DevTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 
-// Adicionando o redux-thunk e o redux-promise ao store
 const store = createStore(
   reducers,
-  applyMiddleware(promise, thunk)  // Aqui você inclui o redux-thunk
+  applyMiddleware(promise, thunk)  
 );
 
 ReactDOM.render(
